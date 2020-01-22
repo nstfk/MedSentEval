d0297fd159d0bcbe4ded3a530017c67362af3a9f
nstfk <nohaseddik@hotmail.com>
2020-01-22 03:35:13 +0200

    Update mednli.py

--- senteval/mednli.py
+++ senteval/mednli.py
@@ -30,42 +30,41 @@ class MedNLIEval(object):
         trainlabels = io.open(os.path.join(taskpath, 'labels.train'),
                               encoding='utf-8').read().splitlines()
         trainids = io.open(os.path.join(taskpath, 'ids.train'),
-                              encoding='utf-8').read().splitlines()
-        
+                           encoding='utf-8').read().splitlines()
 
         valid1 = self.loadFile(os.path.join(taskpath, 's1.dev'))
         valid2 = self.loadFile(os.path.join(taskpath, 's2.dev'))
         validlabels = io.open(os.path.join(taskpath, 'labels.dev'),
                               encoding='utf-8').read().splitlines()
-        
+
         validids = io.open(os.path.join(taskpath, 'ids.dev'),
-                              encoding='utf-8').read().splitlines()
+                           encoding='utf-8').read().splitlines()
 
         test1 = self.loadFile(os.path.join(taskpath, 's1.test'))
         test2 = self.loadFile(os.path.join(taskpath, 's2.test'))
         testlabels = io.open(os.path.join(taskpath, 'labels.test'),
                              encoding='utf-8').read().splitlines()
         testids = io.open(os.path.join(taskpath, 'ids.test'),
-                              encoding='utf-8').read().splitlines()
-        
+                          encoding='utf-8').read().splitlines()
+
         # sort data (by s2 first) to reduce padding
-        sorted_train = sorted(zip(train2, train1, trainlabels,trainids),
-                              key=lambda z: (len(z[0]), len(z[1]), z[2],z[3]))
+        sorted_train = sorted(zip(train2, train1, trainlabels, trainids),
+                              key=lambda z: (len(z[0]), len(z[1]), z[2], z[3]))
         train2, train1, trainlabels, trainids = map(list, zip(*sorted_train))
 
-        sorted_valid = sorted(zip(valid2, valid1, validlabels,validids),
-                              key=lambda z: (len(z[0]), len(z[1]), z[2],z[3]))
+        sorted_valid = sorted(zip(valid2, valid1, validlabels, validids),
+                              key=lambda z: (len(z[0]), len(z[1]), z[2], z[3]))
         valid2, valid1, validlabels, validids = map(list, zip(*sorted_valid))
 
-        sorted_test = sorted(zip(test2, test1, testlabels,testids),
-                             key=lambda z: (len(z[0]), len(z[1]), z[2],z[3]))
-        test2, test1, testlabels,testids = map(list, zip(*sorted_test))
-        #print(testids)
+        sorted_test = sorted(zip(test2, test1, testlabels, testids),
+                             key=lambda z: (len(z[0]), len(z[1]), z[2], z[3]))
+        test2, test1, testlabels, testids = map(list, zip(*sorted_test))
+        # print(testids)
 
         self.samples = train1 + train2 + valid1 + valid2 + test1 + test2
-        self.data = {'train': (train1, train2, trainlabels,trainids),
-                     'valid': (valid1, valid2, validlabels,validids),
-                     'test': (test1, test2, testlabels,testids)
+        self.data = {'train': (train1, train2, trainlabels, trainids),
+                     'valid': (valid1, valid2, validlabels, validids),
+                     'test': (test1, test2, testlabels, testids)
                      }
 
     def do_prepare(self, params, prepare):
@@ -78,17 +77,17 @@ class MedNLIEval(object):
 
     def run(self, params, batcher):
         self.X, self.y = {}, {}
-        dico_label = {'entailment': 0,  'neutral': 1, 'contradiction': 2}
+        dico_label = {'entailment': 0, 'neutral': 1, 'contradiction': 2}
         for key in self.data:
             if key not in self.X:
                 self.X[key] = []
             if key not in self.y:
                 self.y[key] = []
 
-            input1, input2, mylabels,ids = self.data[key]
+            input1, input2, mylabels, ids = self.data[key]
             enc_input = []
             n_labels = len(mylabels)
-            for ii in range(0,n_labels, params.batch_size):
+            for ii in range(0, n_labels, params.batch_size):
                 batch1 = input1[ii:ii + params.batch_size]
                 batch2 = input2[ii:ii + params.batch_size]
 
@@ -97,7 +96,7 @@ class MedNLIEval(object):
                     enc2 = batcher(params, batch2)
                     enc_input.append(np.hstack((enc1, enc2, enc1 * enc2,
                                                 np.abs(enc1 - enc2))))
-                if (ii*params.batch_size) % (200*params.batch_size) == 0:
+                if (ii * params.batch_size) % (200 * params.batch_size) == 0:
                     logging.info("PROGRESS (encoding): %.2f%%" %
                                  (100 * ii / n_labels))
             self.X[key] = np.vstack(enc_input)
@@ -106,7 +105,6 @@ class MedNLIEval(object):
             except:
                 logging.info(' key error')
                 continue
-
 
         config = {'nclasses': 3, 'seed': self.seed,
                   'usepytorch': params.usepytorch,
@@ -119,31 +117,27 @@ class MedNLIEval(object):
         config['classifier'] = config_classifier
 
         clf = SplitClassifier(self.X, self.y, config)
-        
-        devacc, testacc,yhat,probs= clf.run()
-        
-        
+
+        devacc, testacc, yhat, probs = clf.run()
+
         print(self.data['test'][0])
         print(self.data['test'][1])
         print(pred)
         print(probs)
-		pp=[]
-		i=0
-		while i<52:
-  #print(i)
-			pp.append([probs[i],probs[i+1],probs[i+2]])
-			i=i+3
-		dico_label = {0:'entailment',1: 'neutral',2:'contradiction'}
-		predictions=[]
-		for i in preds:
-			predictions.append(dico_label[i[0]])
+        pp = []
+        i = 0
+        while i < 52:
+            pp.append([probs[i], probs[i + 1], probs[i + 2]])
+            i = i + 3
+        dico_label = {0: 'entailment', 1: 'neutral', 2: 'contradiction'}
+        predictions = []
+        for i in preds:
+            predictions.append(dico_label[i[0]])
+        for i, j, k, l in zip(pp, predictions, s1, s2):
+            print(" ".join(k), "\t", " ".join(l), "\t", j, "\t", i)
 
-		for i,j,k,l in zip(pp,predictions,s1,s2):
-			print(" ".join(k),"\t"," ".join(l),"\t",j,"\t",i)
-        
-       
         logging.debug('Dev acc : {0} Test acc : {1} for MedNLI\n'
-                      .format(devacc, testacc))
+                  .format(devacc, testacc))
         return {'devacc': devacc, 'acc': testacc,
-                'ndev': len(self.data['valid'][0]),
-                'ntest': len(self.data['test'][0])}
+            'ndev': len(self.data['valid'][0]),
+            'ntest': len(self.data['test'][0])}
