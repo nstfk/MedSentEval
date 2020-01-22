d5589f65973712416fb510587cd1a8b0630c6a48
nstfk <nohaseddik@hotmail.com>
2020-01-22 03:27:50 +0200

    Update validation.py

--- senteval/tools/validation.py
+++ senteval/tools/validation.py
@@ -238,4 +238,4 @@ class SplitClassifier(object):
         print(pred)
         testaccuracy = clf.score(self.X['test'], self.y['test'])
         testaccuracy = round(100*testaccuracy, 2)
-        return devaccuracy, testaccuracy,yhat,probs
+        return devaccuracy, testaccuracy,preds,probs
